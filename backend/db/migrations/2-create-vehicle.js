9d3544dcbf1e5d2429f1de9a5e7fedabadf0e8d2
Farhad Koushan <farhadkoushan@gmail.com>
2022-12-21 16:45:55 -0800

    options missing from migrations

--- backend/db/migrations/2-create-vehicle.js
+++ backend/db/migrations/2-create-vehicle.js
@@ -109,7 +109,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   down: async (queryInterface, Sequelize) => {
     options.tableName = "Vehicles";
